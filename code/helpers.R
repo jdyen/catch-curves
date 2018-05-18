3d80df20a5e3be21b7e10ac80687f124b383c9de
jdyen <jdl.yen@gmail.com>
2018-05-18 17:44:22 +1000

    format data for greta analysis and add covars

--- code/helpers.R
+++ code/helpers.R
@@ -32,3 +32,24 @@ calc_flow_pc <- function(flow, scale = FALSE) {
   pc_out
   
 }
+
+create_greta_covar <- function(n_sp, n_age,
+                               eta_sp = 1, eta_age = 4,
+                               sigma_mean = 0.0, sigma_sd = 1.0) {
+
+  if (length(sigma_mean) == 1)
+    sigma_mean <- rep(sigma_mean, 2)
+  if (length(sigma_sd) == 1)
+    sigma_sd <- rep(sigma_sd, 2)
+  
+  species_corr_est <- lkj_correlation(eta = eta_sp, dim = n_sp)
+  age_corr_est <- lkj_correlation(eta = eta_age, dim = n_age)
+  species_sigma <- lognormal(mean = sigma_mean[1], sd = sigma_sd[1], dim = n_sp)
+  age_sigma <- lognormal(mean = sigma_mean[2], sd = sigma_sd[2], dim = n_age)
+  species_covar_est <- (species_sigma %*% t(species_sigma)) * species_corr_est
+  age_covar_est <- age_sigma %*% t(age_sigma) * age_corr_est
+  covar_est <- kronecker(species_covar_est, age_covar_est)
+  
+  covar_est
+
+}
--- code/main.R
+++ code/main.R
@@ -3,6 +3,7 @@ setwd("~/Dropbox/research/catch-curves/")
 
 # load packages
 library(lubridate)
+library(greta)
 
 # source helper functions
 source("./code/length_to_mass_calculations.R")
@@ -15,14 +16,39 @@ source("./code/load-flow-data.R")
 source("./code/load-data.R")
 
 # check corrs in lagged flow data
-# remove_correlated(mc_catch_curve$flow)
+# mc_catch_curve$flow_ind <- remove_correlated(mc_catch_curve$flow)
 
 # OR: use PCA to calculate a few flow components
+## NOTE: this gives different PCs to each species -- very similar but
+##       might affect interpretability.
+##       Alternative is to pre-calc PCs and then assign (but this won't
+##       preserve uncorrelated variables)
 mc_pc <- calc_flow_pc(mc_catch_curve$flow, scale = FALSE)
-mc_catch_curve$flow_pc <- mc_pc$scores[, 1:3]
+mc_catch_curve$flow_pc <- mc_pc$scores[, seq_len(3)]
+tc_pc <- calc_flow_pc(tc_catch_curve$flow, scale = FALSE)
+tc_catch_curve$flow_pc <- tc_pc$scores[, seq_len(3)]
+gp_pc <- calc_flow_pc(gp_catch_curve$flow, scale = FALSE)
+gp_catch_curve$flow_pc <- gp_pc$scores[, seq_len(3)]
+sp_pc <- calc_flow_pc(sp_catch_curve$flow, scale = FALSE)
+sp_catch_curve$flow_pc <- sp_pc$scores[, seq_len(3)]
 
 # set up mvn flow model (only have MC, TC, GP, SP) {could use size classes and add rainbows}
-# size x species correlation matrix?
-#  size_corr %x% spp_corr
+# age x species correlation matrix?
+#  age_corr %x% spp_corr
+num_ages <- 6
+all_data <- list(mc_catch_curve,
+                 tc_catch_curve,
+                 gp_catch_curve,
+                 sp_catch_curve)
+age_data <- do.call("rbind", sapply(all_data, function(x) x$age_dist[, seq_len(num_ages)]))
+flow_data <- do.call("rbind", sapply(all_data, function(x) x$flow_pc))
+flow_data <- scale(flow_data)
+info_data <- do.call("rbind", sapply(all_data, function(x) as.matrix(x$info)))
+info_data <- as.data.frame(info_data)
+info_data$spp <- rep(c("MC", "TC", "GP", "SP"),
+                     times = sapply(all_data, function(x) nrow(x$age_dist)))
 
+# create a separable covariance matrix with spp and age components
+covar_mat <- create_greta_covar(n_sp = length(unique(info_data$spp)),
+                                n_age = ncol(age_data))
 
